a6823778c3b99537957264d5000a97eaacc8887b
annavasylashko <anna.vasylashko123@gmail.com>
2023-06-06 19:02:20 +0300

    Feature/integration (#5)
    * Handle unauthorized users
    
    * Integrating advanced selection mechanics
    
    * prettify
    
    * Add logout and settings
    
    * prettify

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public', {
-    extensions: ['html', 'htm']
-}));
+app.use(
+  express.static('public', {
+    extensions: ['html', 'htm'],
+  })
+);
 
 // Define routes
 app.get('/', (req, res) => {
--- public/scripts/navigation.js
+++ public/scripts/navigation.js
@@ -1,43 +1,43 @@
 function loadContent(tabId) {
-    const contentContainer = document.getElementById('content-container');
-    const contentBlocks = contentContainer.children;
+  const contentContainer = document.getElementById('content-container');
+  const contentBlocks = contentContainer.children;
 
-    for (let i = 0; i < contentBlocks.length; i++) {
-        const block = contentBlocks[i];
-        if (block.id === `${tabId}-content`) {
-            block.style.display = 'block';
-        } else {
-            block.style.display = 'none';
-        }
+  for (let i = 0; i < contentBlocks.length; i++) {
+    const block = contentBlocks[i];
+    if (block.id === `${tabId}-content`) {
+      block.style.display = 'block';
+    } else {
+      block.style.display = 'none';
     }
+  }
 }
 
 function initializeNavigation() {
-    const contentContainer = document.getElementById('content-container');
+  const contentContainer = document.getElementById('content-container');
 
-    const roomsTab = document.getElementById('rooms-tab');
-    roomsTab.addEventListener('click', () => {
-        contentContainer.style.display = 'block';
-        loadContent('rooms');
-    });
+  const roomsTab = document.getElementById('rooms-tab');
+  roomsTab.addEventListener('click', () => {
+    contentContainer.style.display = 'block';
+    loadContent('rooms');
+  });
 
-    const gameTab = document.getElementById('game-tab');
-    gameTab.addEventListener('click', () => {
-        contentContainer.style.display = 'block';
-        loadContent('game');
-    });
+  const gameTab = document.getElementById('game-tab');
+  gameTab.addEventListener('click', () => {
+    contentContainer.style.display = 'block';
+    loadContent('game');
+  });
 
-    const profileTab = document.getElementById('profile-tab');
-    profileTab.addEventListener('click', () => {
-        contentContainer.style.display = 'block';
-        loadContent('profile');
-    });
+  const profileTab = document.getElementById('profile-tab');
+  profileTab.addEventListener('click', () => {
+    contentContainer.style.display = 'block';
+    loadContent('profile');
+  });
 
-    const settingsTab = document.getElementById('settings-tab');
-    settingsTab.addEventListener('click', () => {
-        contentContainer.style.display = 'block';
-        loadContent('settings');
-    });
+  const settingsTab = document.getElementById('settings-tab');
+  settingsTab.addEventListener('click', () => {
+    contentContainer.style.display = 'block';
+    loadContent('settings');
+  });
 }
 
 window.addEventListener('load', initializeNavigation);
